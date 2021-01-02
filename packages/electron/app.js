8f15a8bb89776679ed29bb8eac865943d6e13495
Adam Avramov <adam.b.avramov@gmail.com>
2021-01-02 19:56:16 +0100

    rename internal packages and add electron wrapper

---
+++ packages/electron/app.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env electron
+
+'use strict';
+
+const {app, BrowserWindow, Menu} = require('electron')
+const {is} = require('electron-util')
+const unhandled = require('electron-unhandled')
+const debug = require('electron-debug')
+const contextMenu = require('electron-context-menu')
+
+const config = require('./config')
+const menu = require('./menu')
+
+unhandled()
+debug()
+contextMenu()
+
+const windows = {
+  viewer: undefined,
+  editor: undefined
+}
+
+const createViewer = async () => {
+  const win = new BrowserWindow({
+    title:  app.name,
+    show:   false,
+    frame:  false,
+    width:  512,
+    height: 512,
+    webPreferences: { nodeIntegration: true }
+  })
+  win.on('ready-to-show', () => win.show())
+  win.on('closed', () => {windows.viewer = undefined})
+  await win.loadFile(require('path').join(__dirname, 'app/viewer.html'))
+  return win
+}
+
+const createEditor = async () => {
+  const win = new BrowserWindow({
+    title:  app.name,
+    show:   false,
+    frame:  false,
+    width:  512,
+    height: 512,
+    webPreferences: { nodeIntegration: true }
+  })
+  win.on('ready-to-show', () => win.show())
+  win.on('closed', () => {windows.viewer = undefined})
+  await win.loadFile(require('path').join(__dirname, 'app/editor.html'))
+  return win
+}
+
+// Prevent multiple instances of the app
+if (!app.requestSingleInstanceLock()) app.quit()
+app.on('second-instance', () => {
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore()
+    mainWindow.show()
+  }
+})
+
+app.on('window-all-closed', () => {
+  if (!is.macos) app.quit()
+})
+
+const createWindows = async () => {
+  windows.viewer = windows.viewer || await createViewer()
+  windows.editor = windows.editor || await createEditor()
+}
+
+app.on('activate', createWindows)
+
+;(async () => {
+  await app.whenReady()
+  Menu.setApplicationMenu(menu)
+  await createWindows()
+})()
+
