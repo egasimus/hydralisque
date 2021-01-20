15b0d7213b3d9d77c90956551453a822c18eafdc
Adam Avramov <adam.b.avramov@gmail.com>
2021-01-21 00:51:15 +0100

    basic keybindings

--- electron/editor.js
+++ electron/editor.js
@@ -1,12 +1,44 @@
 function initEditor ({host, element, repl}) {
-  const editor = new (require('hydra/hydra-server/app/src/editor.js'))(element)
-  const menu = new (require('hydra/hydra-server/app/src/menu.js'))({ editor, hydra })
-  const gallery = menu.sketches = new (require('hydra/hydra-server/app/src/gallery.js'))(code => {
-    editor.setValue(code)
-    repl.eval(code)
-  })
-  host.onkeydown = require('./keymaps.js')({
-    history: host.history, editor, gallery, menu, repl, log
-  })
+
+  window.hush   = ()=>{}
+  window.render = ()=>{}
+  window.o0     = null
+
+  const editor = new (require('hydra/editor.js'))(element)
+
+  const hydra = {
+    eval (code) {
+      console.log('eval', code)
+    }
+  }
+
+  const menu = new (require('hydra/menu.js'))({ editor, hydra })
+
+  const gallery = menu.sketches = new (require('hydra/gallery.js'))(
+    code => {
+      editor.setValue(code)
+      hydra.eval(code)
+    }
+  )
+
+  host.onkeydown = ({ctrlKey, altKey, shiftKey, metaKey, key}) => {
+    console.log(ctrlKey, altKey, shiftKey, metaKey, key)
+    if (ctrlKey && shiftKey && key === 'Enter') {
+       hydra.eval(editor.getValue())
+    }
+    if (ctrlKey && key === 'Enter') {
+       hydra.eval(editor.getLine())
+    }
+  }
+
+  //require('hydra/keymaps.js')({
+    //history: host.history,
+    //editor,
+    //gallery,
+    //menu,
+    //repl,
+    //log: console.log
+  //})
+
   return editor
 }
