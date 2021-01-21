14db5a010e58b62679dcc7df83fe2fa6ac89d8c7
Adam Avramov <adam.b.avramov@gmail.com>
2021-01-22 00:39:37 +0100

    fix for yarn

--- shell.nix
+++ shell.nix
@@ -2,11 +2,18 @@ let pkgs = import <nixpkgs> {}; in pkgs.stdenv.mkDerivation rec {
   name = "hydralisque";
   propagatedBuildInputs = with pkgs; [
     nodejs-14_x
-    nodePackages.npm
     electron_11
+    yarn
+
+    nodePackages.npm
     nodePackages.node2nix
+
+    pkg-config
+    xlibs.libX11
+    xlibs.libXi
+    xlibs.libXext
   ];
-  buildInputs = pkgs.electron_11.buildInputs;
+  buildInputs = [ pkgs.electron_11.buildInputs ];
   shellHook = ''
     export PATH=`pwd`/node_modules/.bin:$PATH
   '';
